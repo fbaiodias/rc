985b7a657745f999305687fdabff2260ea6cc42d
Francisco Baio Dias <xicombd@gmail.com>
2014-09-22 20:37:19 +0100

    Initial commit

---
+++ CentralServer.java
@@ -0,0 +1,34 @@
+import java.io.*; 
+import java.net.*; 
+
+class CentralServer 
+{
+  public static int PORT = 9876;
+  
+  public static void main(String args[]) throws Exception 
+  {
+    DatagramSocket serverSocket = new DatagramSocket(PORT);
+    byte[] receiveData = new byte[1024];
+    byte[] sendData = new byte[1024];      
+
+    System.out.println("Starting UDP server at port " + PORT);
+
+    while(true) 
+    {
+      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+      serverSocket.receive(receivePacket);
+
+      String sentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
+      System.out.println("RECEIVED: " + sentence);
+
+      InetAddress IPAddress = receivePacket.getAddress();
+      int port = receivePacket.getPort();
+
+      String capitalizedSentence = sentence.toUpperCase();
+      sendData = capitalizedSentence.getBytes();
+
+      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+      serverSocket.send(sendPacket);
+    }
+  } 
+}
